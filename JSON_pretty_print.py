1a1246524ecd5c8175233bd19f2ccbd35b64b709
ShawnDriscoll <shawndriscoll@hotmail.com>
2023-08-20 00:20:51 -0700

    Update JSON_pretty_print.py

--- JSON_pretty_print.py
+++ JSON_pretty_print.py
@@ -2,7 +2,7 @@
 import json
 import pprint
 
-with open('Strange Bedfellows Characters/Sample Char.tps', 'r') as json_file:
+with open('We Want Soviet Men Characters/Sample Char.tps', 'r') as json_file:
     data = json.load(json_file)
 
     pprint.pprint(data)
