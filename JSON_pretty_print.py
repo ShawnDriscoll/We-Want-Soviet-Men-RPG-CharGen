372fcd788db0d607dcb9436e81e5d6e667be0b09
ShawnDriscoll <shawndriscoll@hotmail.com>
2023-08-20 00:18:20 -0700

    Update JSON_pretty_print.py

--- JSON_pretty_print.py
+++ JSON_pretty_print.py
@@ -2,7 +2,7 @@
 import json
 import pprint
 
-with open('Planet Matriarchy Characters/Sample Char.tps', 'r') as json_file:
+with open('Strange Bedfellows Characters/Sample Char.tps', 'r') as json_file:
     data = json.load(json_file)
 
     pprint.pprint(data)
